9b6086350532d334444db63d5b12b38cb5f6d2b1
Abhra Basak <abhra.1992@live.com>
2013-03-26 21:58:29 +0530

    GearedOx Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gearedox::Application.config.secret_token = '8b223cf5d03c28a4f62491dcde201e9c11fc6090b1ef69f7158c48c302ddaebcf1de9d42560f742f96910e5167aa9378be3875abad1e0bf20fe4f77cb8ab4461'

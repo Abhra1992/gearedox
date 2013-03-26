b21754cabc624b9d9db8a7157e6ba410e70262e3
Abhra Basak <abhra.1992@live.com>
2013-03-27 00:59:07 +0530

    Added model for FileType

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130326191319) do
+ActiveRecord::Schema.define(:version => 20130326192829) do
+
+  create_table "file_types", :force => true do |t|
+    t.string   "mime"
+    t.string   "extension"
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "firstname"
